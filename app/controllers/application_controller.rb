bc5bb49eca9df2b7e6f07c9efa59bbf11468f469
Rohini Kalia <rohinikalia@gmail.com>
2014-11-06 21:26:16 -0500

    update gem file; add new actions to application controller; alter root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello
+  	render text: "hello, world!"
+  end
+
+  def hola
+  	render text: "&iexcl;hola, mondo!"
+  end
+
+  def goodbye
+  	render text: "goodbye"
+  end
+
 end
